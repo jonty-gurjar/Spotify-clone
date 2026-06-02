2439b98fa4408fc9afda2c5eb37df662d482a6c1
jonty-gurjar <jeetdhabhai18@gmail.com>
2026-06-02 13:16:36 +0530

    spotify-clone

--- spotify-clone/vite.config.js
+++ spotify-clone/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: process.env.VITE_BASE_PATH || "/Spotify-clone",
+export default defineConfig(() => ({
+  base: globalThis.process?.env.VERCEL ? '/' : (globalThis.process?.env.VITE_BASE_PATH || '/Spotify-clone/'),
   plugins: [react(), tailwindcss()],
 }))
