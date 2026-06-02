f6ab5aa427737b12ded8013311605b404ee816eb
jonty-gurjar <jeetdhabhai18@gmail.com>
2026-06-02 13:00:19 +0530

    spotify-clone-upload

--- spotify-clone/src/main.jsx
+++ spotify-clone/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <PlayerContextProvider>
       <App />
     </PlayerContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
