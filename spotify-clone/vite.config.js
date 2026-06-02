c513204def0660d0105724903ae43d954f44020f
jonty-gurjar <jeetdhabhai18@gmail.com>
2026-06-02 13:12:18 +0530

    fix

--- spotify-clone/vite.config.js
+++ spotify-clone/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Spotify-clone/' : '/',
+  base: process.env.VITE_BASE_PATH || "/Spotify-clone",
   plugins: [react(), tailwindcss()],
 }))
